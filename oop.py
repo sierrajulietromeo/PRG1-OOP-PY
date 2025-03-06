b07271701871ed8f219cf02d56a1a73ee7c7c7e5
Steve Rich <steven.john.rich@gmail.com>
2025-03-06 16:43:20 +0000

    Tidy up formatting. Change valid_positive_integer_name

--- oop.py
+++ oop.py
@@ -1,11 +1,10 @@
-import re # use python's built-in reg expressions module
+import re # optional if you want to use it
 from datetime import datetime # use python's built-in datetime module and only import the datetime class from the datatime module ( the double name is confusing, otherwise we will need to use datetime.datetime.now() )
 
 PRIORITY = { "LOW": 1, "MEDIUM": 3, "HIGH": 5, "URGENT": 7 };
 
 
-
-def valid_integer(value): # value can be a string or an integer
+def valid_positive_integer(value): # value can be a string or an integer
   pass # replace with function logic
 
 
@@ -17,12 +16,9 @@ def todays_date():
   pass # replace with function logic
 
 
-
-
-class Task: 
+class TaskList: 
   pass # replace with class logic
-  # (title, priority)
+  
 
-
-class ToDo: 
+class Todo: 
   pass # replace with class logic
